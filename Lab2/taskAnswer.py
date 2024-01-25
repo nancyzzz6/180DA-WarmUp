38b38aad3b22447be7dedb58bb750e4a2ee7a940
Nancy Zhao <nancyzzz@Nancys-MacBook-Pro.local>
2024-01-25 14:24:11 -0800

    lab 2

---
+++ Lab2/taskAnswer.py
@@ -0,0 +1,16 @@
+# 1. MQTT can be used for multiple players' connections in the prjoject.
+
+#2 - When I tried more similar sounding words, the results didn't show 
+# well as before. In noisy situation, the performance was even worse
+
+# (a)what can you with your given speech program in the project?
+# The speech program can be a way the player use to intereact with the system
+
+# (b) How complex do you want your speech recognition to be? How complex can you reasonably expect your speech recognition to be?
+# I don't think the speech recognition will be too complex for our project because it's not mainly a word game
+
+# (c) What level of speech accuracy do you need? In other words, how quickly do you need an accurate recognition? Does a missed recognition hurt the progress of the game?
+# I think the accuracy should be above 90%, and I hope the recognition within 5 sec. A missed recognition should not hurt the progress of the game
+
+# (d) Do you need specific hardware, specific conditions, etc. to have a reasonable confidence that it works well enough?
+# I think we will need a relative quiet room and maybe a usb microphone.
